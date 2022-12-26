33834d7b2f0544c492693f336022d921777315a5
DigvijayDheer <digvijaydheersaini@gmail.com>
2022-12-26 21:28:48 +0530

    last commit

---
+++ src/Utility/Location.js
@@ -0,0 +1,34 @@
+const GOOGLE_API_KEY = "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg";
+
+export async function getAddressFromCoords(coords) {
+	const response = await fetch(
+		`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${GOOGLE_API_KEY}`
+	);
+	if (!response.ok) {
+		throw new Error("Failed to fetch address. Please try again!");
+	}
+	const data = await response.json();
+	if (data.error_message) {
+		throw new Error(data.error_message);
+	}
+	const address = data.result[0].formatted_address;
+	return address;
+}
+
+export async function getCoordsFromAddress(address) {
+	const urlAddress = encodeURI(address);
+	const response = await fetch(
+		`https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress}&key=${GOOGLE_API_KEY}`
+	);
+
+	if (!response.ok) {
+		throw new Error("Failed to fetch coordinates. Please try again!");
+	}
+	const data = await response.json();
+	if (data.error_message) {
+		throw new Error(data.error_message);
+	}
+
+	const coordinates = data.results[0].geometry.location;
+	return coordinates;
+}
